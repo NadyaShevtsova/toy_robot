198e3d94b7132b22b29bbb75749fd2b8d1cd5daf
Nadiia Shevtsova <nadya.shevtsova@gmail.com>
2016-07-31 20:14:22 +1000

    refactored board_spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,37 +6,28 @@ describe Board do
   context "valid_coordinates? " do
     context "with invalid data should return false " do
       context "with 'x or y coordinate is not a number' message" do
-        it do
-          expect( Board.valid_coordinates? nil ).to eq( false )
-          expect{ Board.valid_coordinates? nil }.to output("x or y coordinate is not a number. Please enter valid command.\n").to_stdout
-        end
-        it do
-          expect( Board.valid_coordinates? [] ).to eq( false )
-          expect{ Board.valid_coordinates? [] }.to output("x or y coordinate is not a number. Please enter valid command.\n").to_stdout
-        end
-        it do
-          expect( Board.valid_coordinates? [ "", "" ] ).to eq( false )
-          expect{ Board.valid_coordinates? [ "", "" ] }.to output("x or y coordinate is not a number. Please enter valid command.\n").to_stdout
-        end
-        it do
-          expect( Board.valid_coordinates? [ "a", "b" ] ).to eq( false )
-          expect{ Board.valid_coordinates? [ "a", "b" ] }.to output("x or y coordinate is not a number. Please enter valid command.\n").to_stdout
-        end
+        it { expect( Board.valid_coordinates? nil ).to eq( false ) }
+        it { expect{ Board.valid_coordinates? nil }.to output("x or y coordinate is not a number. Please enter valid command.\n").to_stdout }
+
+        it { expect( Board.valid_coordinates? [] ).to eq( false ) }
+        it { expect{ Board.valid_coordinates? [] }.to output("x or y coordinate is not a number. Please enter valid command.\n").to_stdout }
+
+        it { expect( Board.valid_coordinates? [ "", "" ] ).to eq( false ) }
+        it { expect{ Board.valid_coordinates? [ "", "" ] }.to output("x or y coordinate is not a number. Please enter valid command.\n").to_stdout }
+
+        it { expect( Board.valid_coordinates? [ "a", "b" ] ).to eq( false ) }
+        it { expect{ Board.valid_coordinates? [ "a", "b" ] }.to output("x or y coordinate is not a number. Please enter valid command.\n").to_stdout }
       end
 
       context "with 'x, y coordinates are not on board' message" do
-        it do
-          expect( Board.valid_coordinates? [ "-1", "0" ] ).to eq( false )
-          expect{ Board.valid_coordinates? [ "-1", "0" ] }.to output("x, y coordinates are not on board. Please enter valid command.\n").to_stdout
-        end
-        it do
-          expect( Board.valid_coordinates? [ "0", "-1" ] ).to eq( false )
-          expect{ Board.valid_coordinates? [ "0", "-1" ] }.to output("x, y coordinates are not on board. Please enter valid command.\n").to_stdout
-        end
-        it do
-          expect( Board.valid_coordinates? [ "5", "5" ] ).to eq( false )
-          expect{ Board.valid_coordinates? [ "5", "5" ] }.to output("x, y coordinates are not on board. Please enter valid command.\n").to_stdout
-        end
+        it { expect( Board.valid_coordinates? [ "-1", "0" ] ).to eq( false ) }
+        it { expect{ Board.valid_coordinates? [ "-1", "0" ] }.to output("x, y coordinates are not on board. Please enter valid command.\n").to_stdout }
+
+        it { expect( Board.valid_coordinates? [ "0", "-1" ] ).to eq( false ) }
+        it { expect{ Board.valid_coordinates? [ "0", "-1" ] }.to output("x, y coordinates are not on board. Please enter valid command.\n").to_stdout }
+
+        it { expect( Board.valid_coordinates? [ "5", "5" ] ).to eq( false ) }
+        it { expect{ Board.valid_coordinates? [ "5", "5" ] }.to output("x, y coordinates are not on board. Please enter valid command.\n").to_stdout }
       end
     end
 
